f0b33c4d92cae9a225210d1fce94a77c60892c0d
lukaskoepping <lukask@panoleon.de>
2021-12-18 11:47:34 +0100

    add create param name to switch dbs

--- tap-server/src/database.ts
+++ tap-server/src/database.ts
@@ -5,72 +5,95 @@ import {
     createRxDatabase,
     getRxStoragePouch,
     RxDatabase,
-    RxDatabaseGenerated
-} from "rxdb";
-import {RxDBServerPlugin} from 'rxdb/plugins/server';
-import pouchdb_adapter_node_websql from 'pouchdb-adapter-node-websql';
-import {RxDatabaseBase} from "rxdb/dist/types/rx-database";
-import {AthleteSchema} from "../../shared/schemas/athlete.schema";
-import {OfficialSchema} from "../../shared/schemas/official.schema";
-import {TeamSchema} from "../../shared/schemas/team.schema";
-import {AcroSchema} from "../../shared/schemas/acro.schema";
-import {AppointmentSchema} from "../../shared/schemas/appointment.schema";
+    RxDatabaseGenerated,
+} from 'rxdb'
+import { RxDBServerPlugin } from 'rxdb/plugins/server'
+import pouchdb_adapter_node_websql from 'pouchdb-adapter-node-websql'
+import { RxDatabaseBase } from 'rxdb/dist/types/rx-database'
+import { AthleteSchema } from '../../shared/schemas/athlete.schema'
+import { OfficialSchema } from '../../shared/schemas/official.schema'
+import { TeamSchema } from '../../shared/schemas/team.schema'
+import { AcroSchema } from '../../shared/schemas/acro.schema'
+import { AppointmentSchema } from '../../shared/schemas/appointment.schema'
 
-
-addRxPlugin(RxDBServerPlugin);
-addPouchPlugin(pouchdb_adapter_node_websql);
-
+addRxPlugin(RxDBServerPlugin)
+addPouchPlugin(pouchdb_adapter_node_websql)
 
 export class Database {
+    private static dbPromise: RxDatabaseBase<any, any> &
+        CollectionsOfDatabase &
+        RxDatabaseGenerated<CollectionsOfDatabase>
 
-    private static dbPromise: RxDatabaseBase<any, any> & CollectionsOfDatabase & RxDatabaseGenerated<CollectionsOfDatabase>;
+    private static dbList = new Map<string, RxDatabase>()
 
-    public static get = async () => {
-        if (!Database.dbPromise) {
-            Database.dbPromise = await Database.createDatabase();
+    public static async get(name = 'base'): Promise<RxDatabase> {
+        // get database from dbList by name
+        var db = this.dbList.get(name)
+        if (db != null) {
+            // return db if exist
+            return db
         }
-        return Database.dbPromise;
-    };
 
-    private static async createDatabase() {
+        // if not exist -> create database
+        var base = name == 'base'
+        return this.createDatabase(name, base)
+    }
 
-        const db: RxDatabase = await createRxDatabase({
-            name: 'db',
+    private static async createDatabase(name = 'base', base = false) {
+        var db: RxDatabase = await createRxDatabase({
+            name: './db' + name,
             storage: getRxStoragePouch('websql'),
             ignoreDuplicate: true,
-        });
+        })
 
-        await db.waitForLeadership();
-        console.log('isLeader now');
+        await db.waitForLeadership()
+        console.log('isLeader now')
 
         try {
-
-            await db.addCollections({
-                athletes: {
-                    schema: AthleteSchema
-                },
-                teams: {
-                    schema: TeamSchema
-                },
-                officials: {
-                    schema: OfficialSchema
-                },
-                acros: {
-                    schema: AcroSchema
-                },
-                appointments: {
-                    schema: AppointmentSchema
-                },
-            });
-
+            if (base) {
+                await db.addCollections({
+                    athletes: {
+                        schema: AthleteSchema,
+                    },
+                    teams: {
+                        schema: TeamSchema,
+                    },
+                    officials: {
+                        schema: OfficialSchema,
+                    },
+                    acros: {
+                        schema: AcroSchema,
+                    },
+                    appointments: {
+                        schema: AppointmentSchema,
+                    },
+                })
+            } else {
+                await db.addCollections({
+                    athletes: {
+                        schema: AthleteSchema,
+                    },
+                    teams: {
+                        schema: TeamSchema,
+                    },
+                    officials: {
+                        schema: OfficialSchema,
+                    },
+                    acros: {
+                        schema: AcroSchema,
+                    },
+                    appointments: {
+                        schema: AppointmentSchema,
+                    },
+                })
+            }
         } catch (e) {
-            console.log('error: ', e);
+            console.log('error: ', e)
         }
 
-        console.log('db initialized.')
-        return db;
+        this.dbList.set(name, db)
+
+        console.log(name + ' initialized.')
+        return db
     }
 }
-
-
-
