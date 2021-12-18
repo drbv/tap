f2f9e862b19b1671a2402509a48a73b7c7985e34
lukaskoepping <lukask@panoleon.de>
2021-12-18 14:19:27 +0100

    add turnament-list in ui

--- tap-ui/src/pages/dashboard/Dashboard.jsx
+++ tap-ui/src/pages/dashboard/Dashboard.jsx
@@ -1,22 +1,33 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
-import { LinearProgress } from '@material-ui/core'
+import MUIDataTable from 'mui-datatables'
+import {
+    LinearProgress,
+    Paper,
+    Button,
+    Tooltip,
+    IconButton,
+    Typography,
+} from '@material-ui/core'
+import { TipsAndUpdates } from '@mui/icons-material'
 import withStyles from '@material-ui/core/es/styles/withStyles'
+import withProps from '../../components/HOC'
+import { getBaseCollection, closeCollection } from '../../Database'
 
 const styles = (theme) => ({
     root: {
         flexGrow: 1,
         padding: 30,
     },
+    table: {
+        margin: '11px',
+        marginBottom: '80px',
+    },
+    fetchAppointments: {
+        margin: 10,
+    },
     paper: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-        width: '16rem',
-        height: '16rem',
-        '&:hover': {
-            cursor: 'pointer',
-        },
+        margin: '11px',
     },
     logo: {
         maxWidth: '10rem',
@@ -38,11 +49,129 @@ class Dashboard extends Component {
     constructor(props) {
         super(props)
 
-        this.state = {}
+        this.state = {
+            Turnaments: null,
+        }
+
+        this.subs = []
+    }
+
+    async componentDidMount() {
+        let collection = await getBaseCollection('appointments')
+        let sub = await collection.find().$.subscribe((Turnaments) => {
+            if (!Turnaments) {
+                return
+            }
+            console.log('reload Tournament-list ')
+            console.dir(Turnaments)
+            this.setState({
+                Turnaments,
+            })
+        })
+
+        this.subs.push(sub)
     }
 
     render() {
-        return <div>Dashboard</div>
+        const { classes } = this.props
+        const { Turnaments } = this.state
+        return (
+            <div>
+                <Paper className={classes.table}>
+                    <Button
+                        className={classes.fetchAppointments}
+                        color="inherit"
+                        variant="outlined"
+                        color="primary"
+                        onClick={() => {
+                            this.setState({ newCoupleOpen: true })
+                        }}
+                    >
+                        Turnierliste aktualisieren
+                    </Button>
+                    <Typography variant="text" color="initial">
+                        Ausgewähltes Turnier:
+                    </Typography>
+                </Paper>
+                {Turnaments != null ? (
+                    <MUIDataTable
+                        className={classes.table}
+                        data={Turnaments}
+                        columns={[
+                            {
+                                name: 'appointment_id',
+                                options: {
+                                    filter: false,
+                                },
+                                name: 'location',
+                                options: {
+                                    filter: false,
+                                    sort: true,
+                                },
+                                name: 'isActive',
+                                options: {
+                                    filter: false,
+                                    sort: true,
+                                },
+                                name: 'createState',
+                                options: {
+                                    filter: false,
+                                    sort: true,
+                                },
+                            },
+                            {
+                                name: 'Aktionen',
+                                options: {
+                                    sort: false,
+                                    customBodyRender: (
+                                        value,
+                                        tableMeta,
+                                        updateValue
+                                    ) => {
+                                        if (tableMeta.rowData != null) {
+                                            return (
+                                                <div>
+                                                    <Tooltip title="Aktivieren">
+                                                        <span>
+                                                            <IconButton
+                                                                onClick={() => {
+                                                                    fetch(
+                                                                        'https://localhost:5000/activate/' +
+                                                                            tableMeta
+                                                                                .rowData[0]
+                                                                    ).then(
+                                                                        function (
+                                                                            response
+                                                                        ) {
+                                                                            return response.json()
+                                                                        }
+                                                                    )
+                                                                }}
+                                                            >
+                                                                <TipsAndUpdates />
+                                                            </IconButton>
+                                                        </span>
+                                                    </Tooltip>
+                                                </div>
+                                            )
+                                        }
+                                    },
+                                },
+                            },
+                        ]}
+                        options={{
+                            responsive: 'scrollFullHeight',
+                            filter: false,
+                            print: false,
+                            selectableRows: 'none',
+                            rowsPerPageOptions: [10, 25, 50, 100, 250],
+                        }}
+                    />
+                ) : (
+                    <LinearProgress />
+                )}
+            </div>
+        )
     }
 }
 
