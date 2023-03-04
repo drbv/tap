a5089670b0451ea394ed2713e95748caf32599ae
Lukas the graetest <luko6202@th-wildau.de>
2023-03-04 19:01:56 +0100

    add dexie db and live-component on client site

--- tap-ui-ts/src/App.tsx
+++ tap-ui-ts/src/App.tsx
@@ -2,93 +2,79 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import {
-  Box,
-  Button,
-  Card,
-  Chip,
-  Divider,
-  Grid,
-  Paper,
-  Stack,
-  Switch,
-  TextField,
+    Box,
+    Button,
+    Card,
+    Chip,
+    Divider,
+    Grid,
+    Paper,
+    Stack,
+    Switch,
+    TextField,
 } from "@mui/material";
-import { getCollection } from "./Database";
-import { RxDocument, RxDocumentBase, RxQuery } from "rxdb";
+import {getClientDb} from "./Database";
+import {useLiveQuery} from "dexie-react-hooks"
 
 type AppProps = {};
 type AppState = {
-  exchangeObject: any;
-  inputValue: string;
+    exchangeObjects: any;
+    inputValue: string;
 };
 
-class App extends React.Component<AppProps, AppState> {
-  private subs: any[];
+export function ExchangeObjectsList() {
+    const exchangeObjects = useLiveQuery(async () =>
+        getClientDb().exchange.toArray()
+    )
 
-  constructor(props: AppProps) {
-    super(props);
+    return <>
+        {exchangeObjects?.map(exchange => <li key={exchange.id}>
+            {exchange.request}, {exchange.response}
+        </li>)}
+    </>
 
-    this.state = {
-      inputValue: "default",
-      exchangeObject: null,
-    };
-    // initializes subscription array
-    this.subs = [];
-  }
-
-  async componentDidMount() {
-    const exchangeCollection = await getCollection("sampledb", "exchange");
-    const exchangeSubscription = exchangeCollection
-      .findOne("singleexchange")
-      .$.subscribe((exchangeObject: any) => {
-        if (!exchangeObject) {
-          return;
-        } else {
-          console.dir(exchangeObject);
-          this.setState({ exchangeObject });
-        }
-      });
-    this.subs.push(exchangeSubscription);
-  }
-
-  componentWillUnmount() {
-    this.subs.forEach((sub) => sub.unsubscribe());
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Box sx={{ mx: "auto", padding: "20px" }}>
-          <Card sx={{ mx: "auto", padding: "5px" }}>
-            {this.state.exchangeObject
-              ? this.state.exchangeObject.response
-              : "Keine Rückmeldung"}
-            <Divider />
-            <TextField
-              value={this.state.inputValue}
-              onChange={(e) => this.setState({ inputValue: e.target.value })}
-            />
-            <Button
-              variant="contained"
-              onClick={async (e) => {
-                const exchangeCollection = await getCollection(
-                  "sampledb",
-                  "exchange"
-                );
-                await exchangeCollection.upsert({
-                  id: "singleExchange",
-                  request: this.state.inputValue,
-                  response: "...",
-                });
-              }}
-            >
-              Objekt zurücksenden
-            </Button>
-          </Card>
-        </Box>
-      </div>
-    );
-  }
 }
 
-export default App;
+export default class App extends React.Component<AppProps, AppState> {
+    private subs: any[];
+
+    constructor(props: AppProps) {
+        super(props);
+
+        this.state = {
+            inputValue: "default",
+            exchangeObjects: null,
+        };
+        // initializes subscription array
+        this.subs = [];
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Box sx={{mx: "auto", padding: "20px"}}>
+                    <Card sx={{mx: "auto", padding: "5px"}}>
+                        <ExchangeObjectsList/>
+                        <Divider/>
+                        <TextField
+                            value={this.state.inputValue}
+                            onChange={(e) => this.setState({inputValue: e.target.value})}
+                        />
+                        <Button
+                            variant="contained"
+                            onClick={(e) => {
+                                getClientDb().exchange.add({
+                                    id: this.state.inputValue + "singleExchange",
+                                    request: this.state.inputValue,
+                                    response: "...",
+                                });
+                            }}
+                        >
+                            Objekt zurücksenden
+                        </Button>
+                    </Card>
+                </Box>
+            </div>
+        );
+    }
+}
--- tap-ui-ts/src/Database.tsx
+++ tap-ui-ts/src/Database.tsx
@@ -1,99 +1,30 @@
-import {
-  addRxPlugin,
-  createRxDatabase,
-  RxCollectionBase,
-  RxDatabase,
-} from "rxdb";
+import Dexie, {Table} from "dexie"
 
-import { getRxStorageDexie } from "rxdb/plugins/dexie";
-import { RxDBReplicationCouchDBNewPlugin } from "rxdb/plugins/replication-couchdb-new";
+let dbPromise: DexieDatabase;
 
-addRxPlugin(RxDBReplicationCouchDBNewPlugin);
-
-let dbPromise: RxDatabase<any>;
-const activeSyncs: Map<{ database: string; collection: string }, any> =
-  new Map();
-
-/**
- * @description This function returns a RxCollection that is synchronized with a server database
- * @param collection name of the collection that is synchronized and returned
- * @param database name of the database the collection is synchronized to
- */
-export async function getCollection(database: string, collection: string) {
-  const clientDB: RxDatabase = await getClientDb();
-  const rxCollection: RxCollectionBase<any> = clientDB[collection];
-
-  // sync local collection with server
-  const repState = await rxCollection.syncCouchDBNew({
-    url: "http://localhost:5001/sampledb/",
-    live: true,
-  });
-
-  repState.active$.subscribe((status) => {
-    console.log("Sync is active: " + status);
-  });
-
-  // save repState to be able to close sync
-  activeSyncs.set({ database, collection }, repState);
-
-  return rxCollection;
+export interface Exchange {
+    id?: string;
+    request: string;
+    response: string;
 }
 
-/**
- * @description closeCollection is a function that allows the user to manually close a sync
- * @param collection name of the collection which synchronization is closed
- */
-export async function closeCollection(
-  database: string = "sampledb",
-  collection: string
-) {
-  const repState = activeSyncs.get({ database, collection });
-  if (repState) {
-    await repState.cancel();
-    activeSyncs.delete({ database, collection });
-  }
-}
+export class DexieDatabase extends Dexie {
+    // 'friends' is added by dexie when declaring the stores()
+    // We just tell the typing system this is the case
+    exchange!: Table<Exchange>;
 
-/**
- * @description This function creates a new database and adding collectionsS
- */
-async function _create() {
-  const db = await createRxDatabase({
-    name: "clientdb",
-    storage: getRxStorageDexie(),
-    ignoreDuplicate: true,
-  });
-
-  await db.addCollections({
-    exchange: {
-      schema: {
-        title: "exchange",
-        description: "exchange object",
-        version: 0,
-        primaryKey: "id",
-        type: "object",
-        properties: {
-          id: {
-            type: "string",
-            final: true,
-          },
-          request: {
-            type: "string",
-          },
-          response: {
-            type: "string",
-          },
-        },
-      },
-    },
-  });
-  return db;
+    constructor() {
+        super('myDatabase');
+        this.version(1).stores({
+            exchange: '++id, request, response' // Primary key and indexed props
+        });
+    }
 }
 
 /**
  * @description this function returns the client database or create if not exist
  */
-export async function getClientDb() {
-  if (!dbPromise) dbPromise = await _create();
-  return dbPromise;
+export function getClientDb() {
+    if (!dbPromise) dbPromise = new DexieDatabase();
+    return dbPromise;
 }
