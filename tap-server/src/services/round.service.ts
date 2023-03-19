8758193c9b887cd12dec29bf61be4dcd45452346
lukaskoepping <lukask@panoleon.de>
2023-03-19 13:10:35 +0100

    upgrade dependencies

--- tap-server/src/services/round.service.ts
+++ tap-server/src/services/round.service.ts
@@ -2,18 +2,18 @@ import {RoundWorkflowService} from "../workflows/services/roundWorkflowService";
 import {Subscription} from "rxjs";
 import {Database} from "../database";
 import {Context} from "../workflows/workflow/round-context.model";
+import {getClientDb} from "../../../tap-ui-ts/src/Database";
 
 export class RoundService {
 
     private context = new Context();
     private roundWorkflowService: RoundWorkflowService = new RoundWorkflowService(this.context);
-    private subscription: Subscription;
 
     public createRound() {
         this.context.message = "change context here with data from db";
         Database.getSampleDB().then(db => {
             console.log('subscribing...')
-            this.subscription = db.sampleCollection.$.subscribe((data: any) => this.onCreateRound(data));
+            this.onCreateRound(db.exchange.toArray()[0]);
             this.roundWorkflowService.createRound();
         });
     }
